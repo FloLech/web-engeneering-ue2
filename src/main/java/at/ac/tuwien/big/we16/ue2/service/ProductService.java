6c1857f795f8851e6b6521dac933d1e4d06ce14c
Florian Lechleitner <lechleitner@gmx.net>
2016-04-25 00:01:47 +0200

    Final Submission

--- src/main/java/at/ac/tuwien/big/we16/ue2/service/ProductService.java
+++ src/main/java/at/ac/tuwien/big/we16/ue2/service/ProductService.java
@@ -17,15 +17,15 @@ public class ProductService {
 
     static {
         Product[] checkRuntime = {
-                new Product(1l, "Forty Licks", 0.0, null, "resources/images/rolling_stones.png", null, "2016,04,24,20,06,20,000"),
-                new Product(2l, "The Martian", 0.0, null, "resources/images/the_martian.png", null, "2016,04,24,04,43,19,000"),
-                new Product(3l, "The Godfather", 0.0, null, "resources/images/the_godfather.png", null, "2016,04,24,20,08,19,000"),
-                new Product(4L, "Metallica", 0.0, null, "resources/images/metallica.png", null,  "2016,04,28,14,54,20,000"),
-                new Product(5L, "Red Hot Chili Peppers", 0.0, null, "resources/images/red_hot_chili_peppers.png", null, "2016,04,24,17,08,19,000"),
-                new Product(6L, "Pride and Prejudice", 0.0, null, "resources/images/pride_and_prejudice.png", null, "2016,04,25,17,08,19,000"),
-                new Product(7L, "The Wizard of Oz", 0.0, null, "resources/images/the_wizard_of_oz.png", null, "2016,04,24,17,08,19,000"),
-                new Product(8L, "The Great Gatsby", 0.0, null, "resources/images/the_great_gatsby.png", null, "2016,04,24,17,08,19,000"),
-                new Product(9L, "1984", 0.0, null, "resources/images/1984.png", null, "2016,04,25,17,08,19,796")};
+                new Product(1l, "Forty Licks", 0.0, null, "images/rolling_stones.png", null, "2016,04,24,20,06,20,000"),
+                new Product(2l, "The Martian", 0.0, null, "images/the_martian.png", null, "2016,04,24,04,43,19,000"),
+                new Product(3l, "The Godfather", 0.0, null, "images/the_godfather.png", null, "2016,04,24,20,08,19,000"),
+                new Product(4L, "Metallica", 0.0, null, "images/metallica.png", null,  "2016,04,28,14,54,20,000"),
+                new Product(5L, "Red Hot Chili Peppers", 0.0, null, "images/red_hot_chili_peppers.png", null, "2016,04,24,17,08,19,000"),
+                new Product(6L, "Pride and Prejudice", 0.0, null, "images/pride_and_prejudice.png", null, "2016,04,25,17,08,19,000"),
+                new Product(7L, "The Wizard of Oz", 0.0, null, "images/the_wizard_of_oz.png", null, "2016,04,24,17,08,19,000"),
+                new Product(8L, "The Great Gatsby", 0.0, null, "images/the_great_gatsby.png", null, "2016,04,24,17,08,19,000"),
+                new Product(9L, "1984", 0.0, null, "images/1984.png", null, "2016,04,25,17,08,19,796")};
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy,MM,dd HH,mm,ss,SSS");
         //get current date time with Date()
         String date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS").format(new Date());
